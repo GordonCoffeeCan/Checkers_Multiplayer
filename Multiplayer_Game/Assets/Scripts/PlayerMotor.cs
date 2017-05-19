85f350d6919ec03950345ba39bd6844e9f26451d
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-05-19 17:29:21 -0400

    set up new project

---
+++ Multiplayer_Game/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Vector3 _velocity) {
+        velocity = _velocity;
+    }
+
+    private void FixedUpdate() {
+        PerformMovement();
+    }
+
+    private void PerformMovement() {
+        if (velocity != Vector3.zero) {
+            rb.MovePosition(rb.position + velocity * Time.deltaTime);
+        }
+    }
+}
