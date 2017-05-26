7a0c79f5e7a8ce340b14fe4d0ded730ee4f8d0ff
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-05-26 15:37:09 -0400

    able to join game through lobby

---
+++ Multiplayer_Game/Assets/Scripts/HostGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour {
+
+    [SerializeField]
+    private uint roomSize = 4;
+
+    private string roomName;
+
+    private NetworkManager networkMangager;
+
+	// Use this for initialization
+	void Start () {
+        networkMangager = NetworkManager.singleton;
+        if(networkMangager.matchMaker == null) {
+            networkMangager.StartMatchMaker();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetRoomName(string _name) {
+        roomName = _name;
+    }
+
+    public void CreateRoom() {
+        if(roomName != "" && roomName != null) {
+            Debug.Log(roomName + ", size " + roomSize);
+
+            //Create room
+            networkMangager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkMangager.OnMatchCreate);
+            
+        }
+    }
+}
---
+++ Multiplayer_Game/Assets/Scripts/RoomListItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
+public class RoomListItem : MonoBehaviour {
+
+    public delegate void JoinRoomDelegate(MatchInfoSnapshot _match);
+    private JoinRoomDelegate joinRoomCallBack;
+
+    [SerializeField]
+    private Text roomNameText;
+
+    private MatchInfoSnapshot match;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Setup(MatchInfoSnapshot _match, JoinRoomDelegate _joinRoomCallBack) {
+        match = _match;
+
+        joinRoomCallBack = _joinRoomCallBack;
+
+        roomNameText.text = match.name + " (" + match.currentSize + "/" + match.maxSize + ")";
+    }
+
+    public void JoinRoom() {
+        joinRoomCallBack.Invoke(match);
+    }
+}
